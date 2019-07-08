18d5b157b84b35c3c9ffc2f351f6e2b9072db553
Krister Looga <looga.krister@gmail.com>
2019-07-08 07:04:40 +0300

    Added checkout

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import './App.css';
+import { Route } from "react-router-dom"
 
 import Layout from "./hoc/Layout/Layout"
 import BurgerBuilder from "./Containers/BurgerBuilder/BurgerBuilder"
-
+import Checkout from "./Containers/Checkout/Checkout"
 
 
 function App() {
     return (
         <div>
             <Layout>
-                <BurgerBuilder/>
+                <Route path={"/checkout"} component={Checkout}/>
+                <Route path={"/"} exact component={BurgerBuilder}/>
             </Layout>
         </div>
     );
