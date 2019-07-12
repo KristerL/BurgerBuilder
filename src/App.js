04aa2b06253b06bf3c5924fc282ae7b0e434981b
krister <looga.krister@gmail.com>
2019-07-12 17:31:35 +0300

    Lazy loading

--- src/App.js
+++ src/App.js
@@ -4,11 +4,23 @@ import {Route, Redirect} from "react-router-dom"
 import Layout from "./hoc/Layout/Layout"
 import BurgerBuilder from "./Containers/BurgerBuilder/BurgerBuilder"
 import Checkout from "./Containers/Checkout/Checkout"
-import Orders from "./Containers/Orders/Orders"
 import Auth from "./Containers/Auth/Auth"
 import Logout from "./Containers/Auth/Logout/Logout"
 import * as actions from "./store/actions/index"
 import {connect} from "react-redux";
+import asyncComponent from "./hoc/asyncComponent/asyncComponent"
+
+const asyncCheckout = asyncComponent(() =>{
+    return import("./Containers/Checkout/Checkout")
+});
+
+const asyncOrder = asyncComponent(() =>{
+    return import("./Containers/Orders/Orders")
+});
+
+const asyncAuth = asyncComponent(() =>{
+    return import("./Containers/Auth/Auth")
+});
 
 class App extends Component {
     componentDidMount() {
@@ -19,7 +31,7 @@ class App extends Component {
 
         let routes = (
             <div>
-                <Route path={"/auth"} component={Auth}/>
+                <Route path={"/auth"} component={asyncAuth}/>
                 <Route path={"/"} exact component={BurgerBuilder}/>
                 <Redirect to={"/"}/>
             </div>
@@ -28,10 +40,10 @@ class App extends Component {
         if (this.props.isAuthenticated) {
             routes = (
                 <div>
-                    <Route path={"/checkout"} component={Checkout}/>
-                    <Route path={"/orders"} component={Orders}/>
+                    <Route path={"/checkout"} component={asyncCheckout}/>
+                    <Route path={"/orders"} component={asyncOrder}/>
                     <Route path={"/logout"} component={Logout}/>
-                    <Route path={"/auth"} component={Auth}/>
+                    <Route path={"/auth"} component={asyncAuth}/>
                     <Route path={"/"} exact component={BurgerBuilder}/>
                     <Redirect to={"/"}/>
                 </div>
